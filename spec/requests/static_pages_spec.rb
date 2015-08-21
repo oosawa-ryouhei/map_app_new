aaeb7c95301d92949997be5b855717159904832d
roosawa <si12005@edu.aomori-u.ac.jp>
2015-08-21 07:07:08 +0000

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,18 +18,4 @@ describe "Static pages" do
   #   it { should have_content('Help') }
   #   it { should have_title(full_title('Help')) }
   # end
-
-  describe "About page" do
-    before { visit about_path }
-
-    it { should have_content('About') }
-    it { should have_title(full_title('About Us')) }
-  end
-
-  describe "Contact page" do
-    before { visit contact_path }
-
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
-  end
 end
